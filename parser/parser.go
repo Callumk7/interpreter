5d1632f8e9746566a531ee22729a26adcc0bf90d
Callum Kloos <callumkloos@gmail.com>
2024-08-04 13:45:26 +0100

    add error checking

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"monkey-interpreter/ast"
 	"monkey-interpreter/lexer"
 	"monkey-interpreter/token"
@@ -11,79 +12,92 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
-    p.nextToken()
-    p.nextToken()
+	// we call this twice, to get a token for cur and peek
+	p.nextToken()
+	p.nextToken()
 
-    return p
+	return p
+}
+
+// return the errors
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) nextToken() {
-    p.curToken = p.peekToken
-    p.peekToken = p.l.NextToken()
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-    program := &ast.Program{}
-    program.Statements = []ast.Statement{}
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
 
-    for p.curToken.Type != token.EOF {
-        stmt := p.parseStatement()
-        if stmt != nil {
-            program.Statements = append(program.Statements, stmt)
-        }
-        p.nextToken()
-    }
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
 
-    return program
+	return program
 }
 
 func (p *Parser) parseStatement() ast.Statement {
-    switch p.curToken.Type {
-    case token.LET:
-        return p.parseLetStatement()
-    default:
-        return nil
-    }
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
-    stmt := &ast.LetStatement{Token: p.curToken}
+	stmt := &ast.LetStatement{Token: p.curToken}
 
-    if !p.expectPeek(token.IDENT) {
-        return nil
-    }
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
 
-    stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 
-    if !p.expectPeek(token.ASSIGN) {
-        return nil
-    }
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
 
-    // TODO: We are skipping the expressions until we encounter a semicolon
-    for !p.curTokenIs(token.SEMICOLON) {
-        p.nextToken()
-    }
+	// TODO: We are skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
 
-    return stmt
+	return stmt
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
-    return p.curToken.Type == t
+	return p.curToken.Type == t
 }
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
-    return p.peekToken.Type == t
+	return p.peekToken.Type == t
 }
 func (p *Parser) expectPeek(t token.TokenType) bool {
-    if p.peekTokenIs(t) {
-        p.nextToken()
-        return true
-    } else {
-        return false
-    }
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 
